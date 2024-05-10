9ccf10a7cca15c7a8a4ff73999055624428ea251
EdsonCampanhao <edsonlemos0525@gmail.com>
2024-05-10 14:47:29 -0300

    new component
    -Header;

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,27 @@
 import styles from './Header.module.css';
+import { IoMdCart } from "react-icons/io";
+
 
 export default function Header(){
     return(
-        <h1 className={styles.titulo}>foi?</h1>
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <img className={styles.logo} size={32} alt='logo da empresa' src='/img/logo.svg'/>
+                <ul className={styles.lista}>
+                    <li className={styles.lista_item}>
+                        <a className={styles.lista_item_link}>Calculadora de materiais</a>
+                    </li>
+                    <li className={styles.lista_item}>
+                        <a className={styles.lista_item_link}> produtos </a>
+                    </li>
+                    <li className={styles.lista_item}>
+                        <a className={styles.lista_item_link}> institucional </a>
+                    </li>
+                </ul>
+                <a>
+                    <IoMdCart size={32} className={styles.carrinho}/>
+                </a>
+            </div>
+        </header>
     )
 }
