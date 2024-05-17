1b3cb689ee3aefb26b6a25b9b5c80a537de1e39d
EdsonCampanhao <edsonlemos0525@gmail.com>
2024-05-17 09:26:12 -0300

    adding route links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,8 +7,40 @@ import Link from 'next/link';
 
 export default function Header(){
 
-    const verifyRouteActive = (event) =>{
-        console.log(event.target)       
+    const activeRoute = (event) =>{
+        const links = event.target.parentNode.parentNode.children
+        const linksA = Array.from(links)
+
+        linksA.forEach((a)=>{
+            if(a.classList.contains('active')){
+                a.classList.toggle('active')
+                a.style.backgroundColor='';
+                a.style.color=''
+            }
+            if (a == event.target.parentNode){
+                a.classList.toggle('active');
+                console.log(a.classList);
+                console.log(a.classList.contains('active'))
+                a.style.backgroundColor='#fff';
+                a.style.color='black'
+            } 
+        })             
+    }
+    const desactiveRoute = (event) =>{
+        const links = event.target.previousElementSibling.children
+        const linksA = Array.from(links)
+
+        
+        linksA.forEach((a)=>{
+            if(a.classList.contains('active')){
+                a.classList.toggle('active')
+                a.style.backgroundColor='';
+                a.style.color=''
+            }
+        })
+        
+
+
     }
 
     return(
@@ -17,18 +49,18 @@ export default function Header(){
                 <img className={styles.logo} size={32} alt='logo da empresa' src='/img/logo.svg'/>
                 <ul className={styles.lista}>
                     <li className={styles.lista_item}>
-                        <Link href='/calculator' onClick={(event)=>{verifyRouteActive(event)}} className={styles.lista_item_link}>Calculadora de materiais</Link >
+                        <Link href='/calculator' onClick={(event)=>{activeRoute(event)}} className={styles.lista_item_link}>Calculadora de materiais</Link >
                     </li>
                     <li className={styles.lista_item}>
-                        <Link href='/products' className={styles.lista_item_link}> produtos </Link >
+                        <Link href='/products' onClick={(event)=>{activeRoute(event)}} className={styles.lista_item_link}> produtos </Link >
                     </li>
                     <li className={styles.lista_item}>
-                        <Link href='/institucional' className={styles.lista_item_link}> institucional </Link >
+                        <Link href='/institucional' onClick={(event)=>{activeRoute(event)}} className={styles.lista_item_link}> institucional </Link >
                     </li>
                 </ul>
 
-                <Link href='/shopping_cart'>
-                    <IoMdCart size={32} className={styles.carrinho}/>
+                <Link href='/shopping_cart' onClick={(event)=>{desactiveRoute(event)}} className={styles.link_cart} >
+                    <IoMdCart  size={32} className={styles.carrinho}/>
                 </Link>
                 <MenuHamburguer></MenuHamburguer>
             </div>
