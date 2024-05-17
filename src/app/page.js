7fc36d3635d1601c1999756a2d2237c31a7f3ce7
EdsonCampanhao <edsonlemos0525@gmail.com>
2024-05-17 11:52:05 -0300

    product carousel

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,51 @@
-import Image from "next/image";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import Card_feedback from "@/components/Card_feedback";
-import Product_card from "@/components/Product_card";
-import Link from "next/link";
+import style from './Home.module.css'
+import { IoDiamondOutline } from "react-icons/io5";
+import { TbPigMoney } from "react-icons/tb";
+import { SlEarphonesAlt } from "react-icons/sl";
+import Product_card from '@/components/Product_card';
+import products from '@/components/products';
+
 
 
 
 export default function Home() {
   return (
     <main >
-        
-        
+
+      <section className={style.container}>
+        <img src='/img/banner.png' className={style.banner} />
+      </section>
+
+      <section className={style.container_differences}>
+        <ul className={style.differences}>
+          <li className={style.differences_item}>
+            <IoDiamondOutline size={32}/>
+            <p className={style.differences_text}>Produtos de qualidade</p>
+          </li>
+          <li className={style.differences_item}>
+            <TbPigMoney size={32} />
+            <p className={style.differences_text}>Preço competitivo</p>
+          </li>
+          <li className={style.differences_item}>
+            <SlEarphonesAlt size={32} />
+            <p className={style.differences_text}>Atendimento especializado</p>
+          </li>
+        </ul>
+      </section>
+
+      <section className={style.container_products}>
+
+        <ul className={style.product_wrapper}>
+
+          {Object.keys(products).map((key)=>{
+            return(
+          <Product_card path={products[key].img} key={key} product={products[key].nome}></Product_card>
+          )})
+           }
+
+        </ul>
+       
+      </section>
 
     </main>
   );
---
+++ src/components/products/index.js
@@ -0,0 +1,161 @@
+
+const products={
+    1:{
+        nome:'alicate pucionador',
+        img:'/img/alicate_pucionador.jpeg',
+    },
+    2:{
+        nome:'arame 10',
+        img:'/img/arame10.jpg',
+    },
+    3:{
+        nome:'arame 18',
+        img:'/img/arame18.jpg',
+    },
+    4:{
+        nome:'bit para parafusadeira',
+        img:'/img/bit.jpeg',
+    },
+    5:{
+        nome:'caixa de luz',
+        img:'/img/caixa_de_luz.jpeg',
+    },
+    6:{
+        nome:'pino furado',
+        img:'/img/CAIXA_PINO_FURADO.JPEG',
+    },
+    7:{
+        nome:'pino roscado',
+        img:'/img/CAIXA_PINO_ROSCADO.JPEG',
+    },
+    8:{
+        nome:'cantoneira perfurada',
+        img:'/img/cantoneira_perfurada.jpeg',
+    },
+    9:{
+        nome:'cantoneira',
+        img:'/img/cantoneira_perfura.jpeg',
+    },
+    10:{
+        nome:'cerrote',
+        img:'/img/CERROTE.JPEG',
+    },
+    11:{
+        nome:'cizal',
+        img:'/img/CIZAL.jpg',
+    },
+    12:{
+        nome:'desempenadeira',
+        img:'/img/desempenadeira.jpeg',
+    },
+    13:{
+        nome:'espatula',
+        img:'/img/espatula_vonder.jpeg',
+    },
+    14:{
+        nome:'espuma expansiva',
+        img:'/img/espuma_expansiva.jpeg',
+    },
+    15:{
+        nome:'estilete',
+        img:'/img/estilet.jpeg',
+    },
+    16:{
+        nome:'fincapino amarelo',
+        img:'/img/FINCAPINO_AMARELO.JPEG',
+    },
+    17:{
+        nome:'fita cimenticia',
+        img:'/img/fita_cimenticia.jpeg',
+    },
+    18:{
+        nome:'fita telada',
+        img:'/img/fita_telada.jpeg',
+    },
+    19:{
+        nome:'fita dupla face',
+        img:'/img/fita-dupla-face-1.jpg',
+    },
+    20:{
+        nome:'gesso cola',
+        img:'/img/GESSO_COLA.JPEG',
+    },
+    21:{
+        nome:'gesso 40kg',
+        img:'/img/GESSO-removebg-preview.png',
+    },
+    22:{
+        nome:'guia 48',
+        img:'/img/GUIAS.JPEG',
+    },
+    23:{
+        nome:'guia 70',
+        img:'/img/GUIAS.JPEG',
+    },
+    24:{
+        nome:'guia 90',
+        img:'/img/GUIAS.JPEG',
+    },
+    25:{
+        nome:'lã de rocha',
+        img:'/img/larocha.jpeg',
+    },
+    26:{
+        nome:'lã de vidro',
+        img:'/img/lavidro.jpeg',
+    },
+    27:{
+        nome:'massa cimenticia',
+        img:'/img/MASSA_CIMENTICIA.JPEG',
+    },
+    28:{
+        nome:'massa drywall',
+        img:'/img/MASSA_DRYWALL.JPEG',
+    },
+    29:{
+        nome:'montante 48',
+        img:'/img/MONTANTES.JPEG',
+    },
+    30:{
+        nome:'montante 70',
+        img:'/img/MONTANTES.JPEG',
+    },
+    31:{
+        nome:'montante 90',
+        img:'/img/MONTANTES.JPEG',
+    },
+    32:{
+        nome:'caixa de parafuso',
+        img:'/img/PARAFUSO.JPEG',
+    },
+    33:{
+        nome:'pino cadeirinha',
+        img:'/img/PINO_CADEIRINHA.JPEG',
+    },
+    34:{
+        nome:'placa cimenticia',
+        img:'/img/PLACA_CIMENTICIA.JPEG',
+    },
+    35:{
+        nome:'placa resistente a umidade',
+        img:'/img/PLACA_RU.JPEG',
+    },
+    36:{
+        nome:'placa st',
+        img:'/img/PLACA_ST.JPEG',
+    },
+    37:{
+        nome:'plaquinha de gesso 60x60',
+        img:'/img/PLAQUINHA.JPEG',
+    },
+    38:{
+        nome:'tabica',
+        img:'/img/tabica.jpeg',
+    },
+    39:{
+        nome:'tirante',
+        img:'/img/TIRANTE.JPEG',
+    },
+}
+
+export default products
