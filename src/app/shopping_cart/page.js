7da9cf45e7ec9052d4e9ec94fcefe334bfc886ba
EdsonCampanhao <edsonlemos0525@gmail.com>
2024-06-07 11:31:05 -0300

    fix
    -fixing cart

--- src/app/shopping_cart/page.js
+++ src/app/shopping_cart/page.js
@@ -2,17 +2,14 @@
 import CardCart from "@/components/CardCart/CardCart"
 import products from "@/components/products"
 import style from './shopping_cart.module.css'
-import { useEffect } from "react"
+
 
 export default function shopping_cart() {
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
             return (
 
                 <div className={style.container}>
 
-                    {
+                    { window?
                         Object.keys(localStorage).map((key) => {
 
                             const obj = JSON.parse(localStorage[parseInt(key)]);
@@ -26,13 +23,12 @@ export default function shopping_cart() {
                                 value={obj[products[key].nome]} />)
 
                         }
-                        )
+                        ):<></>
                     }
                     <CardCart src={products[1].img} alt={products[1].nome} name={products[1].nome} value={3} >  </CardCart>
                 </div>
 
             )
-        }
-    }, []);
+    ;
 
 }
--- src/components/Product_card/index.js
+++ src/components/Product_card/index.js
@@ -3,41 +3,40 @@ import style from './Product_card.module.css';
 import { IoMdCart } from "react-icons/io";
 import { FaRegQuestionCircle } from "react-icons/fa";
 import products from '../products';
-import { useEffect } from 'react';
 
 export default function Product_card(props) {
 
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            return (
-                <div className={style.card}>
-                    <img src={props.path} className={style.product} />
-                    <h3 className={style.product_text}>{props.product}</h3>
+    return (
+        <div className={style.card}>
+            <img src={props.path} className={style.product} />
+            <h3 className={style.product_text}>{props.product}</h3>
 
-                    <div className={style.car}>
-                        <IoMdCart size={32} onClick={() => {
-                            if (Object.keys(localStorage).indexOf(props.product) < 0) {
+            <div className={style.car}>
+                <IoMdCart size={32} onClick={() => {        
+                      if (Object.keys(localStorage).indexOf(props.product) < 0) {
 
-                                for (let product in products) {
-                                    if (products[product].nome == props.product) {
-                                        const item = {}
+                        for (let product in products) {
+                            if (products[product].nome == props.product) {
+                                const item = {}
 
-                                        item[props.product] = 1
+                                item[props.product] = 1
 
-                                        localStorage.setItem(product, JSON.stringify(item));
-                                        break
-                                    }
-                                }
+                                localStorage.setItem(product, JSON.stringify(item));
+                                break
                             }
-                        }} />
-                    </div>
-                    <div className={style.help}>
-                        <FaRegQuestionCircle size={16} />
-                    </div>
-                </div>
-            )
-        }
-    }, []);
+                        }
+                    }
+
+                    
+
+                }} />
+            </div>
+            <div className={style.help}>
+                <FaRegQuestionCircle size={16} />
+            </div>
+        </div>
+    )
+
 
 }
