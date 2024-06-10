7967939cdd78b18d9cc18072ca386096c5824e47
EdsonCampanhao <edsonlemos0525@gmail.com>
2024-06-10 16:06:52 -0300

    fix
    -design of form;

--- src/app/calculator/page.js
+++ src/app/calculator/page.js
@@ -19,33 +19,44 @@ export default function calculator() {
                     <img src='/img/icon_label.png' className={style.img}></img>
                 </div>
 
-                
-
-                    {stateForm == false ?
-                        <>
-                            <select id='select' title="Escolha uma opção" className={style.form_field} required>
-                                <option value="teto" key="teto"> serviço de teto </option>
-                                <option value="parede" key="parede"> serviço de parede </option>
-                            </select>
-                            <button className={style.button} onClick={(event) => {
-                                event.preventDefault();
-                                let typeSelected = () => { return document.querySelector('#select').value }
-                                setTypeForm(typeSelected())
-
-                                setStateForm(true)
-                                console.log(typeForm)
-
-                            }}>
-                                <FaArrowAltCircleRight size={24} fill='rgb(136, 135, 135)' />
-                            </button>
-                        </>
-                        : typeForm == 'parede' ? <FormParede></FormParede> : <FormTeto></FormTeto>
-
-                    }
 
 
+                {stateForm == false ?
 
-                
+                    <>
+
+
+                        <div className={style.container_select} >
+                            <label className={style.label}>qual será o tipo de serviço?
+                                <select required id='select' className={style.select} >
+                                    <option value="teto" key="teto"> teto </option>
+                                    <option value="parede" key="parede"> parede </option>
+                                </select>
+                            </label>
+                        </div>
+
+
+                        
+
+                        <button className={style.button} onClick={(event) => {
+                            event.preventDefault();
+                            let typeSelected = () => { return document.querySelector('#select').value }
+                            setTypeForm(typeSelected())
+
+                            setStateForm(true)
+                            console.log(typeForm)
+
+                        }}>
+                            <FaArrowAltCircleRight size={24} fill='rgb(136, 135, 135)' />
+                        </button>
+                    </>
+                    : typeForm == 'parede' ? <FormParede></FormParede> : <FormTeto></FormTeto>
+
+                }
+
+
+
+
 
             </section>
 
